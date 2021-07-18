c678ad37427af3a852badefc8bc481e851b0866c
Brendan Robert <brendan.robert50point@gmail.com>
2021-07-18 09:58:47 -0400

    added stuff

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# run `chmod 744` file first
+sudo apt-get install nodejs -y
+mkdir site
+git clone https://github.com/brendan-robert-1/tutor-site.git
+npm install express
+node  ./tutor-site/server.js
